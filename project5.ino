c9d651df0ad5c0aeb1d54264be8c9419a2774bc9
akansha-singh5 <85220304+akansha-singh5@users.noreply.github.com>
2021-07-07 22:17:36 +0530

    Add files via upload

---
+++ project5.ino
@@ -0,0 +1,25 @@
+//sa re ga ma generator
+
+void setup()
+{
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  tone(12, 240, 1000); 
+  delay(1000); 
+  tone(12, 270, 1000); 
+  delay(1000); 
+  tone(12, 300, 1000); 
+  delay(1000); 
+  tone(12, 320, 1000); 
+  delay(1000); 
+  tone(12, 360, 1000); 
+  delay(1000); 
+  tone(12, 400, 1000); 
+  delay(1000); 
+  tone(12, 450, 1000); 
+  delay(1000); 
+  
+}
